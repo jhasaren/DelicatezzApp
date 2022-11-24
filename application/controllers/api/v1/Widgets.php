bb699b715e852ecb706b0855314bdc0f3df8f808
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-11-23 23:23:58 -0500

    Actualizacion mensaje echoping

--- application/controllers/api/v1/Widgets.php
+++ application/controllers/api/v1/Widgets.php
@@ -32,7 +32,7 @@ class Widgets extends REST_Controller {
         // Set the response and exit
         $this->response([
             'status' => 1,
-            'message' => 'Servicio en funcionamiento, gracias'
+            'message' => 'Servicio en funcionamiento correctamente'
         ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
 
     }
